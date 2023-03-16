d9a1a55c1b1d8679f7c0f207f3fba58a5b60b9a8
khaledxab <khaledxab@gmail.com>
2023-03-16 12:13:31 +0100

    . Convert to fahrenheit

---
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -0,0 +1,6 @@
+List<double> convertToF(List<double> temperaturesInC) {
+  return temperaturesInC
+      .map((celsius) => ((celsius * 9 / 5) + 32).toStringAsFixed(2))
+      .map(double.parse)
+      .toList();
+}
