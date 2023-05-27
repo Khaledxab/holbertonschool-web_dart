017de8f12a3d59fad010b8beea5f57afd86ecf87
khaledxab <khaledxab@gmail.com>
2023-05-27 16:47:03 +0200

    fix compare

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,29 +1,26 @@
-import 'dart:html';
-
-int getPoints(Map<String, int> team) {
-  int points = 0;
-  team.forEach((key, value) {
-    if (key == 'Free throws') {
-      points += value;
-    } else if (key == '2 pointer') {
-      points += value * 2;
-    } else if (key == '3 pointer') {
-      points += value * 3;
-    }
-  });
-  return points;
-}
-
-int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+int whoWins(Map<String, int?> teamA, Map<String, int?> teamB) {
   int teamAPoints = getPoints(teamA);
   int teamBPoints = getPoints(teamB);
+  
   if (teamAPoints > teamBPoints) {
     return 1;
   } else if (teamAPoints < teamBPoints) {
     return 2;
-  } else if (teamAPoints == teamBPoints) {
-    return 0;
   } else {
-    return -1;
-  } 
+    return 0;
+  }
+}
+
+int getPoints(Map<String, int?> team) {
+  int points = 0;
+  if (team.containsKey('Free throws')) {
+    points += team['Free throws'] ?? 0;
+  }
+  if (team.containsKey('2 pointer')) {
+    points += 2 * (team['2 pointer'] ?? 0);
+  }
+  if (team.containsKey('3 pointer')) {
+    points += 3 * (team['3 pointer'] ?? 0);
+  }
+  return points;
 }
